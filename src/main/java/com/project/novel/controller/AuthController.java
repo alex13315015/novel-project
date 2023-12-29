d36b3d546b78ec7525589ddb3ff79fd6ff5adb9a
alex13315015 <150584133+alex13315015@users.noreply.github.com>
2023-12-29 15:12:05 +0900

    feat: 1.RequiredArgsConstructor 어노테이션 추가 (MemberService 생성자 주입) 2.회원가입 구현

--- src/main/java/com/project/novel/controller/AuthController.java
+++ src/main/java/com/project/novel/controller/AuthController.java
@@ -1,14 +1,21 @@
 package com.project.novel.controller;
 
+import com.project.novel.dto.JoinDto;
+import com.project.novel.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
+    private final MemberService memberService;
     @GetMapping("/login")
     public String login() {
         return "/auth/login";
@@ -16,5 +23,15 @@ public class AuthController {
     @GetMapping("/logout")
     public String logout() {
         return "/auth/logout";
+    }
+    @GetMapping("/join")
+    public String join(Model model) {
+        model.addAttribute("joinDto",new JoinDto());
+        return "/auth/join";
+    }
+    @PostMapping("/join")
+    public String joinProcess(JoinDto joinDto) {
+        memberService.join(joinDto);
+        return "/auth/join";
     }
 }
