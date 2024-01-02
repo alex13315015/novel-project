96188a91fed7e7cfed9b8b5c7ef43bec75956810
alex13315015 <150584133+alex13315015@users.noreply.github.com>
2024-01-02 10:57:40 +0900

    feat: 아이디 중복체크 구현(Controller)

--- src/main/java/com/project/novel/controller/AuthController.java
+++ src/main/java/com/project/novel/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.project.novel.controller;
 
 import com.project.novel.dto.JoinDto;
+import com.project.novel.repository.MemberRepository;
 import com.project.novel.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @Slf4j
@@ -16,14 +21,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/auth")
 public class AuthController {
     private final MemberService memberService;
+    private final MemberRepository memberRepository;
     @GetMapping("/login")
     public String login() {
         return "/auth/login";
     }
-    @GetMapping("/logout")
-    public String logout() {
-        return "/auth/logout";
-    }
+
     @GetMapping("/join")
     public String join(Model model) {
         model.addAttribute("joinDto",new JoinDto());
@@ -33,5 +36,13 @@ public class AuthController {
     public String joinProcess(JoinDto joinDto) {
         memberService.join(joinDto);
         return "/auth/join";
+    }
+    @GetMapping("/idCheck")
+    @ResponseBody
+    public int idCheck(String dupId) {
+        if(dupId == null || dupId.isEmpty())
+            return -1;
+        else
+            return memberService.idCheck(dupId);
     }
 }
