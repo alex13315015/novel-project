124d336207e965cca0c162ccf10ba31f6f554d71
alex13315015 <150584133+alex13315015@users.noreply.github.com>
2024-01-02 11:12:04 +0900

    feat: 아이디 중복체크 하는 service 구현

---
+++ src/main/java/com/project/novel/service/MemberService.java
@@ -0,0 +1,33 @@
+package com.project.novel.service;
+
+import com.project.novel.dto.JoinDto;
+import com.project.novel.entity.Member;
+import com.project.novel.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    public Member join(JoinDto joinDto) {
+        Member dbJoinMember = Member.builder()
+                .userId(joinDto.getUserId())
+                .password(bCryptPasswordEncoder.encode(joinDto.getPassword()))
+                .userName(joinDto.getUserName())
+                .nickName(joinDto.getNickName())
+                .email(joinDto.getEmail())
+                .phoneNumber(joinDto.getPhoneNumber())
+                .age(joinDto.getAge())
+                .adminCheck(false)
+                .build();
+        return memberRepository.save(dbJoinMember);
+    }
+    public int idCheck(String userId) {
+        return memberRepository.checkDuplicatedId(userId);
+    }
+}
