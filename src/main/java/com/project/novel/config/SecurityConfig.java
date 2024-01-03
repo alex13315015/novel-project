53574030995d32b101d3f039bc3f0cb89a12afb3
alex13315015 <alex13315015@gmail.com>
2024-01-03 17:46:23 +0900

    feat: csrf 토큰 설정 추가(보안 강화)

--- src/main/java/com/project/novel/config/SecurityConfig.java
+++ src/main/java/com/project/novel/config/SecurityConfig.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity httpSecurity) throws Exception {
@@ -29,8 +32,9 @@ public class SecurityConfig {
                             .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout"))
                             .logoutSuccessUrl("/auth/login")
                             .invalidateHttpSession(true)
-                    )
-                .csrf((csrf) -> csrf.disable());
+                    ).csrf((csrf) -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                );
 
         return httpSecurity.build();
 
