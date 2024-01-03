8973b632a5fd252c27c178c3b281876c0f955b5a
alex13315015 <alex13315015@gmail.com>
2024-01-03 14:59:38 +0900

    refactor: validation 내용을 JoinDto로 이동

--- src/main/java/com/project/novel/entity/Member.java
+++ src/main/java/com/project/novel/entity/Member.java
@@ -1,10 +1,16 @@
 package com.project.novel.entity;
 
+import com.project.novel.constant.Grade;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.validation.annotation.Validated;
 
 import java.time.LocalDateTime;
 
@@ -21,17 +27,33 @@ public class Member {
     @Column(name = "memberId")
     private int id;
 
+    @Column(nullable = false, unique = true)
     private String userId;
+
+    @Column(nullable = false)
     private String userName;
+
     private String nickName;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false, unique = true)
+    @Email
     private String email;
+
+    @Column(length = 2)
     private Integer age;
+
     private String phoneNumber;
+
     private Integer coin;
 
-    private boolean adminCheck;
-    // 일반 사용자와 관리자를 구분하는 컬럼
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Grade role;
+    // 등급에 따른 role 부여하는 컬럼
+
     @CreatedDate
     private LocalDateTime createdAt;
     // 회원 등록 날짜
