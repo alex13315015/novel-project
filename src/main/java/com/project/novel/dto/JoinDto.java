d149cb0c7225962ba569f1fae21dde4142d39157
alex13315015 <alex13315015@gmail.com>
2024-01-03 14:57:53 +0900

    feat: validation 적용

--- src/main/java/com/project/novel/dto/JoinDto.java
+++ src/main/java/com/project/novel/dto/JoinDto.java
@@ -1,5 +1,7 @@
 package com.project.novel.dto;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -8,12 +10,21 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class JoinDto {
+    @Size(min = 8, max = 20, message = "아이디는 8~20글자만 가능합니다.")
     private String userId;
+    @Size(min = 3, max = 20, message = "이름은 8~20글자만 가능합니다.")
     private String userName;
+    @Size(max = 20, message = "닉네임은 최대 20글자까지 가능합니다.")
     private String nickName;
+
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
+            message = "비밀번호는 문자,숫자,특수문자를 사용하여 최소 8~20글자여야 합니다.")
     private String password;
+
     private String email;
+
     private int age;
+
     private String phoneNumber;
 
 }
