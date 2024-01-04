b43dc753ae4902e08e9dfe50cb3251c4ea7e197d
alex13315015 <alex13315015@gmail.com>
2024-01-04 12:14:49 +0900

    refactor: csrf 토큰 삭제 및 csrf 활성화

--- src/main/java/com/project/novel/config/SecurityConfig.java
+++ src/main/java/com/project/novel/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -32,9 +31,10 @@ public class SecurityConfig {
                             .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout"))
                             .logoutSuccessUrl("/auth/login")
                             .invalidateHttpSession(true)
-                    ).csrf((csrf) -> csrf
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                );
+                    );
+        /*.csrf((csrf) -> csrf.disable()
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())*/
+
 
         return httpSecurity.build();
 
