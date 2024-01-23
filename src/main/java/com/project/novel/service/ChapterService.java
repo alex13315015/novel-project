30622d511e3aa773e241baacda45e3f143b98f0c
jun3442 <lotring@naver.com>
2024-01-23 16:07:53 +0900

    Redis에 조회수 무한으로 올라가는것 세션 당 한번으로 제한

--- src/main/java/com/project/novel/service/ChapterService.java
+++ src/main/java/com/project/novel/service/ChapterService.java
@@ -7,6 +7,7 @@ import com.project.novel.entity.Book;
 import com.project.novel.entity.Chapter;
 import com.project.novel.repository.BookRepository;
 import com.project.novel.repository.ChapterRepository;
+import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,6 +28,7 @@ public class ChapterService {
     private final ChapterRepository chapterRepository;
     private final BookRepository bookRepository;
     private final RedisTemplate<String, Object> redisTemplate;
+    private final HttpSession httpSession;
 
     public void writeChapter(ChapterUploadDto chapterUploadDto, Long bookId) {
         Book book = bookRepository.findById(bookId).orElseThrow(
@@ -48,8 +50,15 @@ public class ChapterService {
                 () -> new IllegalArgumentException("해당하는 챕터를 찾을 수 없습니다.")
         );
 
+        String sessionId = httpSession.getId(); // 세션 ID를 가져옴
+        String viewKey = "chapter:view:" + chapterId + ":" + sessionId;
+
         ValueOperations<String, Object> ops = redisTemplate.opsForValue();
-        ops.increment("chapter:hits:" + chapterId, 1);
+        if (ops.get(viewKey) == null) { // 해당 세션에서 이 챕터를 처음 조회하는 경우
+            ops.set(viewKey, true); // 해당 세션에서 이 챕터를 조회했음을 표시
+            ops.increment("chapter:hits:" + chapterId, 1); // 조회수 증가
+        }
+
         log.info("redis 조회수: " + ops.get("chapter:hits:" + chapterId));
         Number hitsNumber = (Number) ops.get("chapter:hits:" + chapterId);
         Long hits = hitsNumber != null ? hitsNumber.longValue() : chapter.getHits();
@@ -65,7 +74,8 @@ public class ChapterService {
                 .createdAt(formattedCreatedAt)
                 .build();
     }
-    // 1분 마다 업데이트
+
+    @Transactional
     @Scheduled(cron = "0 0/1 * * * *")
     public void updateChapterViews() {
         log.info("updateChapterViews() 실행");
@@ -80,10 +90,13 @@ public class ChapterService {
             Long hits = hitsNumber != null ? hitsNumber.longValue() : null;
 
             if (hits != null) {
+                log.info("챕터 찾는 쿼리 발송 ㅇ");
                 Chapter chapter = chapterRepository.findById(chapterId).orElseThrow(
                         () -> new IllegalArgumentException("해당하는 챕터를 찾을 수 없습니다.")
                 );
+                log.info("hit 업데이트 해주는 쿼리 발송 ㅇ");
                 chapter.updateHits(hits);
+                log.info("save 해주는 쿼리 발송 ㅇ");
                 chapterRepository.save(chapter);
             }
 
