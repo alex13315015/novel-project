fe9b9c3b72d43970a722ed087612a427bf491c8e
jun3442 <lotring@naver.com>
2024-01-31 00:49:53 +0900

    BookGenre 수정 챕터 페이지 기능 수정

--- src/main/java/com/project/novel/enums/BookGenre.java
+++ src/main/java/com/project/novel/enums/BookGenre.java
@@ -1,11 +1,21 @@
 package com.project.novel.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum BookGenre {
-    FANTASY,
-    SCIENCE_FICTION,
-    MYSTERY,
-    THRILLER,
-    ROMANCE,
-    HORROR,
-    ORIENTAL_FANTASY
+    FANTASY("판타지"),
+    SCIENCE_FICTION("과학소설"),
+    MYSTERY("미스터리"),
+    THRILLER("스릴러"),
+    ROMANCE("로맨스"),
+    HORROR("공포"),
+    ORIENTAL_FANTASY("무협");
+
+    private final String displayName;
+
+    BookGenre(String displayName) {
+        this.displayName = displayName;
+    }
+
 }
