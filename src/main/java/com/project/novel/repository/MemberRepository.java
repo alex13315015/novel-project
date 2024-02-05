6b96e2357623d3ce72f52ca3f8d36fa7ce63d316
alex13315015 <alex13315015@gmail.com>
2024-02-05 15:46:10 +0900

    feat: @Repository 어노테이션 설정

--- src/main/java/com/project/novel/repository/MemberRepository.java
+++ src/main/java/com/project/novel/repository/MemberRepository.java
@@ -2,13 +2,15 @@ package com.project.novel.repository;
 
 import com.project.novel.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
-public interface MemberRepository extends JpaRepository<Member,Integer> {
+@Repository
+public interface MemberRepository extends JpaRepository<Member,Long> {
     Optional<Member> findByUserId(String userId);
     Optional<Member> findByEmail(String email);
     Optional<Member> findByPassword(String password);
